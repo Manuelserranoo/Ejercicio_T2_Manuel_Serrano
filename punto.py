1ddaa9ff4b454172fd082e4d39064eed3de4b46d
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2023-03-13 16:58:09 +0100

    añadimos método cuadrante

--- punto.py
+++ punto.py
@@ -3,13 +3,34 @@ class Punto():
         self.x = x
         self.y = y
         #si no se recibe una x y una y se le asigna 0
-    
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+    def cuandrante(self):
+        if self.x > 0 and self.y > 0:
+            return "Primer cuadrante"
+        elif self.x < 0 and self.y > 0:
+            return "Segundo cuadrante"
+        elif self.x < 0 and self.y < 0:
+            return "Tercer cuadrante"
+        elif self.x > 0 and self.y < 0:
+            return "Cuarto cuadrante"
+        elif self.x == 0 and self.y == 0:
+            return "Origen"
+        elif self.x == 0 and self.y != 0:
+            return "Sobre el eje Y"
+        elif self.x != 0 and self.y == 0:
+            return "Sobre el eje X"
+        
 
 
 def main():
     p1 = Punto(2, 3)
-    p2 = Punto(4, 5)
+    p2 = Punto(-4, 5)
     print(p1)
+    print(p1.cuandrante())
+    print(p2)
+    print(p2.cuandrante())
+
     
 if __name__ == '__main__':
     main()
