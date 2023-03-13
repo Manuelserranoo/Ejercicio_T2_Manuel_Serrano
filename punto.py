6b9d6fda728adb29f8995d4d26cc08b7b2f42b4c
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2023-03-13 16:52:12 +0100

    comit

---
+++ punto.py
@@ -0,0 +1,17 @@
+class Punto():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        #si no se recibe una x y una y se le asigna 0
+    
+
+
+def main():
+    p1 = Punto(2, 3)
+    p2 = Punto(4, 5)
+    print(p1)
+    
+if __name__ == '__main__':
+    main()
+
+
