36bc862b782aee1374f6017911a5278497214305
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2023-03-13 17:24:05 +0100

    cambios

--- punto.py
+++ punto.py
@@ -1,3 +1,4 @@
+import math
 class Punto():
     def __init__(self, x, y):
         self.x = x
@@ -20,17 +21,34 @@ class Punto():
             return "Sobre el eje Y"
         elif self.x != 0 and self.y == 0:
             return "Sobre el eje X"
-        
+    def vector(self, pto2):
+        return f'({self.x - pto2.x}, {self.y - pto2.y})'
+    def dist(self, pto2):
+        return f'{math.sqrt(((self.x - pto2.x)**2 + (self.y - pto2.y)**2))}'
 
 
 def main():
-    p1 = Punto(2, 3)
-    p2 = Punto(-4, 5)
-    print(p1)
-    print(p1.cuandrante())
-    print(p2)
-    print(p2.cuandrante())
-
+    A = Punto(2, 3)
+    B = Punto(5, 5)
+    C = Punto(-3, -1)
+    D = Punto(0, 0)
+    print(A)
+    print(A.cuandrante())
+    print(B)
+    print(C)
+    print(C.cuandrante())
+    print(D)
+    print(D.cuandrante())
+    print(A.vector(B))
+    print(B.vector(A))
+    print(A.dist(B))
+    print(B.dist(A))
+    if A.dist(D) < B.dist(D) and A.dist(D) < C.dist(D):
+        print("A esta mas cerca del origen que B y C")
+    elif B.dist(D) < A.dist(D) and B.dist(D) < C.dist(D):
+        print("B esta mas cerca del origen que A y C")
+    elif C.dist(D) < A.dist(D) and C.dist(D) < B.dist(D):
+        print("C esta mas cerca del origen que A y B")
     
 if __name__ == '__main__':
     main()
